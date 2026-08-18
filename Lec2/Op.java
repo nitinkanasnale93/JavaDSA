3487735ea55d06ef3fdf635de1eea42999c988f0
nitinkanasnale <nitinkanasnale3@gmail.com>
2026-08-18 23:37:04 +0530

    Add Lec2 operators

---
+++ Lec2/Op.java
@@ -0,0 +1,32 @@
+public class Op{
+    public static void main(String[] args) {
+        // int a = 10;
+        // int b = 5;
+        // System.out.println("Add = "+(a + b));
+        // System.out.println("Subtract = "+(a - b));
+        // System.out.println("Multiply = "+(a * b));
+        // System.out.println("Divide = "+(a / b));
+        // System.out.println("Modulu = "+(a % b));
+        // System.out.println("Pre Increment Operator");
+        // int a = 10;
+        // int b = ++a;
+        // System.out.println(a);
+        // System.out.println(b);
+        // System.out.println("Post Inc Operator");
+        // int a = 20;
+        // int b = a++;
+        // System.out.println(a);
+        // System.out.println(b);
+        System.out.println("Decrement Operator");
+        // int a = 10;
+        // int b = --a;
+        // System.out.println(a);
+        // System.out.println(b);
+        int a = 10;
+        int b = a--;
+        System.out.println(a);
+        System.out.println(b);
+
+
+    }
+}
---
+++ Lec2/RelationalOperators.java
@@ -0,0 +1,13 @@
+public class RelationalOperators {
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 5;
+        System.out.println("A == B:" + (A == B));
+        System.out.println("A != B:" + (A != B));
+        System.out.println("A > B" + (A > B));
+        System.out.println("A < B:" + (A < B));
+        System.out.println("A >= B:" + (A >= B));
+        System.out.println("A <= B:" + (A <= B));
+    }
+    
+}
